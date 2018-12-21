c98d965daaacf89d76cf238282f0456d89a7d82d
min <lishun@eihoo.com>
2018-12-22 00:20:41 +0800

    init

---
+++ Assets/_Resources/_Scripts/Menu/WEMainMenuClassItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class WEMainMenuClassItem : MonoBehaviour {
+    Dropdown m_Dropdown;
+    public UnityAction<string,string> OnItemClick;
+	// Use this for initialization
+	void Start () {
+        m_Dropdown = GetComponent<Dropdown>();
+        m_Dropdown.onValueChanged.AddListener(OnItemValueChange);
+    }
+	
+	// Update is called once per frame
+	void OnItemValueChange(int nValue) {
+        if (OnItemClick!=null)
+        {
+            OnItemClick(m_Dropdown.captionText.text, m_Dropdown.options[nValue].text);
+        }
+	}
+}
---
+++ Assets/_Resources/_Scripts/Menu/WEMenuItemBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WEMenuItemBase {
+    public string m_strClassName;
+    public string m_strItemName;
+
+    public abstract void OnClick();
+}
